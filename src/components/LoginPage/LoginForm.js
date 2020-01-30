a754834ad82028d8a4289b99d0e88fbc5b86707e
Swarup Mishra <swarup.mishra@happiestminds.com>
2020-01-30 20:09:06 +0530

    added login page

--- src/components/LoginPage/LoginForm.js
+++ src/components/LoginPage/LoginForm.js
@@ -1,55 +1,91 @@
-import React, { Component } from 'react';
-import { Panel, Form, FormGroup, FormControl, Button } from 'react-bootstrap';
+import React from 'react';
+import { Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox } from '@material-ui/core';
+import { Face, Fingerprint } from '@material-ui/icons';
+
+const styles = theme => ({
+    margin: {
+        margin: theme.spacing(2),
+    },
+    padding: {
+        padding: theme.spacing.unit
+    }
+});
 
 
-const divStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  marginTop: -100
-};
+class LoginForm extends React.Component {
+    constructor (args) {
+        super(args);
 
-const panelStyle = {
-  backgroundColor: 'rgba(255,255,255,0.5)',
-  border: 0,
-  paddingLeft: 20,
-  paddingRight: 20,
-  width: 300,
-};
+        this.state = {
+            phone: 0,
+            otpButton: true,
+            loginButton: false
+        };
+    }
 
-const buttonStyle = {
-  marginBottom: 0
-};
+    handleOnChange(e) {
+        this.setState({ phone: e.target.value })
+    }
 
-class LoginForm extends Component {
+    handleOtpButton(){
+        this.setState({otpButton: false, loginButton: true})
+    }
 
-  handleFormSubmit(e) {
-    e.preventDefault();
-
-    console.log("FORM SUBMIT!");
-
-  }
-
-  render() {
-    return (
-      <div style={divStyle}>
-        <Panel style={panelStyle}>
-          <Form horizontal className="LoginForm" id="loginForm">
-            <FormGroup controlId="formEmail">
-              <FormControl type="email" placeholder="Email Address" />
-            </FormGroup>
-            <FormGroup controlId="formPassword">
-              <FormControl type="password" placeholder="Password" />
-            </FormGroup>
-            <FormGroup style={buttonStyle} controlId="formSubmit">
-              <Button bsStyle="primary" type="submit" onClick={this.handleFormSubmit}>
-                Login
-              </Button>
-            </FormGroup>
-          </Form>
-        </Panel>
-      </div>
-    )
-  }
+    render() {
+        const { classes } = this.props;
+        return (
+            <Paper className={classes.padding}>
+                <div className={classes.margin}>
+                    <Grid container spacing={8} alignItems="flex-end">
+                        <Grid item>
+                            <Face />
+                        </Grid>
+                        <Grid item md={true} sm={true} xs={true}>
+                            <TextField id="username" label="Username" type="string" fullWidth autoFocus required />
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={8} alignItems="flex-end">
+                        <Grid item>
+                            <Fingerprint />
+                        </Grid>
+                        <Grid item md={true} sm={true} xs={true}>
+                            <TextField id="number" label="Phone Number" type="number" onChange={this.handleOnChange.bind(this)} fullWidth autoFocus required />
+                        </Grid>
+                    </Grid>
+                    { !this.state.otpButton &&
+                        <Grid container spacing={8} alignItems="flex-end">
+                            <Grid item>
+                                <Fingerprint />
+                            </Grid>
+                            <Grid item md={true} sm={true} xs={true}>
+                                <TextField id="number" label="Otp" type="number" fullWidth required />
+                            </Grid> 
+                        </Grid>
+                    }
+                    <Grid container alignItems="center" justify="space-between">
+                        <Grid item>
+                            <FormControlLabel control={
+                                <Checkbox
+                                    color="primary"
+                                />
+                            } label="Remember me" />
+                        </Grid>
+                        
+                    </Grid>
+                    { this.state.loginButton &&
+                        <Grid container justify="center" style={{ marginTop: '10px' }}>
+                            <Button variant="outlined" color="primary" style={{ textTransform: "none" }}>Login</Button>
+                        </Grid>
+                    }
+                    { this.state.otpButton && this.state.phone.length === 10 &&
+                        <Grid container justify="center" style={{ marginTop: '10px' }}>
+                            <Button variant="outlined" color="primary" style={{ textTransform: "none" }} onClick={this.handleOtpButton.bind(this)}>Get Otp</Button>
+                        </Grid>
+                    }
+                </div>
+            </Paper>
+        );
+    }
 }
 
-export default LoginForm;
+export default withStyles(styles)(LoginForm);
